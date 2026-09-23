5f9b7273c2c7b4b7a7f9b0cb4254c35439280038
Jee <jeecoding09@gmail.com>
2026-09-23 16:45:35 +0800

    feat: configs
    - Added node_modules
    - Added path alias
    - Updated vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,18 @@
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+   plugins: [react(), tailwindcss(), tsconfigPaths()],
+   server: {
+      proxy: {
+         '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            secure: true,
+         },
+      },
+   },
+})
